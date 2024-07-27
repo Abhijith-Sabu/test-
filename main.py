557c9311044024427b47b6a0c5ffd095341e0116
Your Name <your@email.com>
2024-07-27 09:45:19 +0100

    user profile downloader

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('hello')
+import instaloader
+L= instaloader.Instaloader()
+
+
+user_name = input('enter user name ')
+
+user = instaloader.Profile.from_username(L.context,user_name)
+if user:
+  user_profile = L.download_profilepic(user)
+
+  print("the PFP of the user is saved" )
